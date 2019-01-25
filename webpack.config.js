89ad534e6f72314465f8b1f0bb316367946fb752
Sean Bostic <sean.bostic@gmail.com>
2019-01-24 23:45:42 -0500

    Adding plugins

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    devServer: {
+        hot: true,
+        compress: true,
+        contentBase: path.join(__dirname, 'dist'),
+        open: 'Chrome'
+    },
+    watch: true,
+    devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebPackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: "[name].css"
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+}
